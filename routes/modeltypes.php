88615f0212b6cfc4b0a7f954192ba8e5bf7a48e1
Viacheslav Kopaev <viacheslav.kopaev@yandex.ru>
2025-01-04 02:55:08 +0300

    Update modeltypes.php

--- routes/modeltypes.php
+++ routes/modeltypes.php
@@ -9,9 +9,9 @@ use Veneridze\ModelTypes\TypeCollection;
 
 Route::prefix('type')->name('type.')->group(function () {
     Route::get('/{type}/{item}/fields', function (string $type, string $item) {
-        abort_if(!Arr::has(Config::get("model-types"),$type),404);
+        abort_if(!Arr::has(Config::get("model-types"), $type), 404);
         $itemObj = (new TypeCollection($type))->$item;
-        abort_if(!method_exists($itemObj, 'fields'), $type,400);
+        abort_if(!method_exists($itemObj, 'fields'), $type, 400);
         return $itemObj::fields();
-    })->name('type.item.fields');
+    })->name('item.fields');
 });
